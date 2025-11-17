1ec107f9984798e48551773e9edbe9a0b5930888
yali-leshem <125736320+yali-leshem@users.noreply.github.com>
2025-11-17 13:58:06 +0200

    Refactor price calculation constants in calcPrice

--- calcPrice_YaliLeshem.c
+++ calcPrice_YaliLeshem.c
@@ -1,9 +1,14 @@
 /*------------------------------------------------------
-* Filename: calcPrice.c
+* Filename: calcPrice_YaliLeshem.c
 * Description: Find area, weight and price of package by given data of how it's calculated.
 * Author: Yali Leshem
 -------------------------------------------------------*/
 #include <stdio.h>
+
+#define GR_PER_KG (1000)
+#define SQUARED_CM (100)
+#define PRICE_PER_PACK (5.5)
+#define PRICE_PER_KG (11)
 
 int main () {
     int width_mm, length_mm, weight_gr;
@@ -17,14 +22,14 @@ int main () {
     printf("Enter package's weight in grams: \n");
     scanf("%d", &weight_gr);
 
-    double area_squared_cm = (width_mm*length_mm)/100;
-    double weight_kg = weight_gr/1000;
+    double area_squared_cm = (width_mm*length_mm)/SQUARED_CM;
+    double weight_kg = weight_gr/GR_PER_KG;
 
     printf("Total area of the package in cms is: %.2f \n", area_squared_cm); // storing exactly 2 digits after decimal point
 
     printf("Total weight of the package in Kgs is: %.3f \n", weight_kg); // storing exactly 3 digits after decimal point
 
-    printf("Total price of the package is: %.2f \n", 5.5 + 11*((int) weight_kg) + 0.03*((int) area_squared_cm) + 0.01*(area_squared_cm/weight_kg));
+    printf("Total price of the package is: %.2f \n", PRICE_PER_PACK + PRICE_PER_KG*((int) weight_kg) + 0.03*((int) area_squared_cm) + 0.01*(area_squared_cm/weight_kg));
 
     return 0;
 
